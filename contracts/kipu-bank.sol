3c2c6c65cc8d40c66b5ef637aaa1c071e5f7d609
Facundo Criado <141684602+ffacun2@users.noreply.github.com>
2025-10-03 17:30:41 -0300

    Update kipu-bank.sol

--- contracts/kipu-bank.sol
+++ contracts/kipu-bank.sol
@@ -13,19 +13,19 @@ contract KipuBank {
     /*//////////////////////
 
     ///@notice mapping que asocia la direccion del usuario con su saldo en el banco
-    mapping (address usuario => uint256 saldo) private s_saldos;
+    mapping (address user => uint256 balance) private s_balance;
 
     ///@notice Cantidad de depositos que se realizaron en el contrato
-    uint256 public s_cantDepositos;
+    uint256 public s_depositCount;
 
     ///@notice Cantidad de retiros que se realizaron en el contrato
-    uint256 public s_cantRetiros;
+    uint256 public s_withdrawalCount;
 
     ///@notice Limite de Saldo que el banco permite almacenar en total
-    uint256 public immutable i_limitBancario;
+    uint256 public immutable i_bankCapLimit;
 
     ///@notice Limite maximo de saldo que un usuario puede retirar en una transaccion
-    uint256 public immutable i_limitRetiro;
+    uint256 public immutable i_withdrawalLimit;
 
 
     /*//////////////////////
@@ -34,13 +34,13 @@ contract KipuBank {
     
     /// @notice Evento emitido cuando un deposito se realiza con exito.
     /// @param _sender La direccion que realizo el deposito
-    /// @param _cantidad La cantidad de Saldo depositado
-    event KipuBank_Deposito(address _sender, uint256 _cantidad);
+    /// @param _amount La cantidad de Saldo depositado
+    event Deposited(address _sender, uint256 _amount);
 
     /// @notice Evento emitido cuando un retiro se realiza con exito
     /// @param _receiver La direccion que realizo el retiro
-    /// @param _cantidad La cantidad de saldo retirado.
-    event KipuBank_Retiro(address _receiver, uint256 _cantidad);
+    /// @param _amount La cantidad de saldo retirado.
+    event Withdrawn(address _receiver, uint256 _amount);
 
 
     /*//////////////////////
@@ -48,20 +48,24 @@ contract KipuBank {
     /*//////////////////////
 
     /// @notice Error cuando el monto a retirar excede el limite de retiro permitido por transaccion
-    error KipuBank_LimiteExcedidoRetiro (uint256 _cantidad, uint256 _limite);
+    error ExceedsWithdrawLimit (uint256 _amount, uint256 _limit);
 
     /// @notice Error cuando un usuario intenta retirar mas fondos de los que tiene disponible
-    error KipuBank_SaldoInsuficiente (uint256 _requerido, uint256 _disponible);
+    error InsufficientBalance (uint256 _requered, uint256 _availableBalance);
 
     /// @notice Error cuando la cantidad total depositada excede el limite permitido por el banco
-    error KipuBank_LimiteBancarioExcedido (uint256 _cantDeposito, uint256 _cantDisponible, uint256 _limiteBancario);
+    error ExceedsBalanceBankLimit (uint256 _depositAmount, uint256 _availableAmount, uint256 _limitBank);
 
     /// @notice Error cuando el monto de una transaccion es cero
-    error KipuBank_CantCero();
+    error ZeroAmount();
 
-    modifier cantidadCero() {
-        if (msg.value == 0) {
-            revert KipuBank_CantCero();
+    /// @notice Error cuando se envia Ether al contrato sin llamar a la funcion deposit().
+    error DepositNotAllowed();
+
+    /// @dev Modificador que asegura que el valor del parametro de una funcion o msg.value no es cero.
+    modifier nonZeroAmount(uint256 _amount) {
+        if ( _amount == 0) {
+            revert ZeroAmount();
         }
         _;
     }
@@ -71,68 +75,83 @@ contract KipuBank {
             Constructor
     /*//////////////////////
 
-    constructor (uint256 _limiteBancario, uint256 _limiteRetiro) {
-        i_limitBancario = _limiteBancario;
-        i_limitRetiro = _limiteRetiro;
+    constructor (uint256 _bankCapLimit, uint256 _withdrawalLimit) {
+        i_bankCapLimit = _bankCapLimit;
+        i_withdrawalLimit = _withdrawalLimit;
     }
 
+
+    receive () external payable{
+        _handleDeposit(msg.value);
+    }
+
+    fallback () external payable {
+        revert DepositNotAllowed();
+    }
+ 
 
     /*//////////////////////
         Funciones Publicas
     /*//////////////////////
     
     /// @notice Permite a los usuarios retirar saldo de su deposito almacenado.
-    /// @param _cantidad La cantidad de saldo a retirar
-    function retiro (uint256 _cantidad) external {
+    /// @param _amount La cantidad de saldo a retirar
+    function withdrawal(uint256 _amount) external nonZeroAmount(_amount) {
         //check
-        if (_cantidad > i_limitRetiro) {
-                revert KipuBank_LimiteExcedidoRetiro(_cantidad, i_limitRetiro);
+        if (_amount > i_withdrawalLimit) {
+                revert ExceedsWithdrawLimit (_amount, i_withdrawalLimit);
         }
 
-        if (_cantidad > s_saldos[msg.sender]) {
-                revert KipuBank_SaldoInsuficiente (_cantidad, s_saldos[msg.sender]);
+        if (_amount > s_balance[msg.sender]) {
+                revert InsufficientBalance (_amount, s_balance[msg.sender]);
         }
 
         //Effects
-        s_saldos[msg.sender] -= _cantidad;
-        s_cantRetiros++;
+        s_balance[msg.sender] -= _amount;
+        s_withdrawalCount++;
 
         //Interactions
-        (bool success,) = msg.sender.call {value:_cantidad}("");
+        (bool success,) = msg.sender.call {value:_amount}("");
         require(success, "Fallo el envio del saldo");
 
-        emit KipuBank_Retiro(msg.sender, _cantidad);
+        emit Withdrawn(msg.sender, _amount);
     }
 
+    /// @notice 
+    function deposit() external payable nonZeroAmount(msg.value) {
+        _handleDeposit(msg.value);
+    } 
+
     /// @notice Permite a los usuarios depositar saldo en su almacenamiento personal.
-    function depositar () external payable {
+    /// @dev La funcion usa el modificador para chequear el msg.value y luego delega la logica interna
+    function _handleDeposit(uint256 _amount) private {
         //Checks
-        if (address(this).balance > i_limitBancario) {
-                revert KipuBank_LimiteBancarioExcedido (msg.value, address(this).balance - msg.value, i_limitBancario);
+        if (address(this).balance + _amount > i_bankCapLimit) {
+                revert ExceedsBalanceBankLimit (_amount, address(this).balance - _amount, i_bankCapLimit);
         }
 
         //Effects
-        s_saldos[msg.sender] += msg.value;
-        s_cantDepositos++;
+        s_balance[msg.sender] += _amount;
+        s_depositCount++;
 
         //Interactions
-        emit KipuBank_Deposito(msg.sender, msg.value);
+        emit Deposited(msg.sender, _amount);
     }
 
     /// @notice Obtiene el saldo actual de un usuario
     /// @return Saldo del usuario
-    function getSaldo() public view returns (uint256) {
-        return _getSaldo(msg.sender);
+    function getBalance() public view returns (uint256) {
+        return _getBalanceOf(msg.sender);
     }
 
     /*//////////////////////
         Funciones Privadas
     /*//////////////////////
 
-    /// @param _usuario La direccion del usuario
+    /// @param _user La direccion del usuario
     /// @return El saldo del usuario
-    function _getSaldo (address _usuario) private view returns (uint256) {
-        return s_saldos[_usuario];
+    function _getBalanceOf (address _user) private view returns (uint256) {
+        return s_balance[_user];
     }
 
 
